1920cce7df0621910e64984f41639509d79195a8
doranmartin <doranmartin1994@gmail.com>
2023-02-08 12:42:38 -0500

    completed project

--- src/Exercise18_11.java
+++ src/Exercise18_11.java
@@ -1,5 +1,25 @@
-public class Main {
+import java.util.Scanner;
+
+public class Exercise18_11 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Enter an integer: ");
+        Scanner scanner = new Scanner(System.in);
+        long userInput = scanner.nextLong();
+        System.out.printf("The sum of digits in %d is %d\n",
+                userInput, sumDigits(userInput));
     }
+
+    // Sum digits recursive method
+    public static int sumDigits(long n) {
+        return sumDigits(n, 0);
+    }
+
+    // Sum digits tail recursive helper method
+    private static int sumDigits(long n, long sum) {
+        if (n == 0) {
+            return (int)sum;
+        }
+        return sumDigits(n / 10, sum + n % 10);
+    }
+
 }
